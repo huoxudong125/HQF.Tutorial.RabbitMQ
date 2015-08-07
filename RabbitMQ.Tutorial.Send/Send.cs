c8837c6440fd1f6853257ba98eb1075d960d419b
huoxudong125 <423huo@163.com>
2015-08-07 14:59:30 +0800

    Init a first demo for rabbitMQ

---
+++ RabbitMQ.Tutorial.Send/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Tutorial.Send
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    Console.Read();
+                }
+            }
+        }
+    }
+
+}
