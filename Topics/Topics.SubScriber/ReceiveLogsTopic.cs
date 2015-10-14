d6cd7125ffccaee7e37f2aa06813d6ba302a3ff8
huoxudong125 <423huo@163.com>
2015-10-14 13:54:15 +0800

    Add Topics Filter demo.

---
+++ Topics/Topics.SubScriber/ReceiveLogsTopic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Topics.SubScriber
+{
+    internal class ReceiveLogsTopic
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("topic_logs", "topic");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                        Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var bindingKey in args)
+                {
+                    channel.QueueBind(queueName, "topic_logs", bindingKey);
+                }
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                        routingKey,
+                        message);
+                };
+                channel.BasicConsume(queueName, true, consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
